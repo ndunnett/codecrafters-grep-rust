c9e5650a84b7e0b82c7bdac5eaa37c47326ebd19
ndunnett <nathandunnett@me.com>
2024-07-05 11:25:49 +0000

    1

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::process;
 
 fn match_pattern(input_line: &str, pattern: &str) -> bool {
     if pattern.chars().count() == 1 {
-        return input_line.contains(pattern);
+        input_line.contains(pattern)
     } else {
         panic!("Unhandled pattern: {}", pattern)
     }
@@ -26,9 +26,9 @@ fn main() {
     io::stdin().read_line(&mut input_line).unwrap();
 
     // Uncomment this block to pass the first stage
-    // if match_pattern(&input_line, &pattern) {
-    //     process::exit(0)
-    // } else {
-    //     process::exit(1)
-    // }
+    if match_pattern(&input_line, &pattern) {
+        process::exit(0)
+    } else {
+        process::exit(1)
+    }
 }
